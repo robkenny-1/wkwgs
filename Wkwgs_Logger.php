46744a1027cfd41cfa134be0d777d72d28a5b62b
Rob Kenny <rob@robkenny.com>
2018-11-01 09:12:52 -0700

    Added indenting to logging code

--- Wkwgs_Logger.php
+++ Wkwgs_Logger.php
@@ -25,52 +25,113 @@ defined( 'ABSPATH' ) || exit;
 class Wkwgs_Logger 
 {
     const log_file_name = __DIR__ . '/../../../wkwgs.log';
+    static $Indent = 0;
+
+    public static function indent()
+    {
+        return str_repeat( ' ', self::$Indent );
+    }
+    public static function ends_with_eol( $text )
+    {
+        return substr( $text, -1 ) === PHP_EOL;
+    }
     
     public static function clear()
     {
-        $previous_error_handler = set_error_handler( "\Wkwgs_Logger::ignore_file_not_exist_error" );
+        $previous_error_handler = set_error_handler( "self::ignore_file_not_exist_error" );
 
-        unlink( Wkwgs_Logger::log_file_name );
+        unlink( self::log_file_name );
 
         set_error_handler( $previous_error_handler );
     }
-    public static function log_function( $func )
+    public static function log_function( $func, $params = null )
     {
-        $msg = PHP_EOL . "===== Function ===== $func";
-        Wkwgs_Logger::log( $msg );
+        $message = "===== Enter $func";
+        self::log_internal( $message );
+
+        if ( ! empty( $params ) )
+        {
+            $i = 0;
+            foreach ( $params as $param )
+            {
+                $i = $i + 1;
+                self::log_param( $i, $param );
+            }
+        }
     }
-    public static function log_var( $var_name, $var )
+    public static function log_return( $func, $value = null )
     {
-        $msg  = "----- Variable ----- $var_name" . PHP_EOL;
+        if ( isset( $value ) )
+        {
+            self::log_var( 'return', $value );
+        }
+        $message = "===== Exit  $func";
+        self::log_internal( $message );
+    }
+    public static function log_value( $var_name, $var, $prefix = 'Variable' )
+    {
         if ( isset( $var ) )
         {
             if ( gettype( $var ) === 'boolean' )
             {
-                $msg .= $var ? 'True' : 'False';
+                $var = $var ? 'True' : 'False';
             }
             else
             {
-                $msg .= print_r( $var, true );
+                $var = print_r( $var, true );
             }
         }
         else
         {
-            $msg .= "Type = (unset)" . PHP_EOL;
+            $var = "(unset)";
         }
-        Wkwgs_Logger::log( $msg );
+        $message   = "----- $prefix $var_name = $var" . PHP_EOL;
+        self::log_internal( $message );
     }
-    public static function log_msg( $msg )
+    public static function log_var( $var_name, $var )
     {
-        $msg = "------ Message ----- $func";
-        Wkwgs_Logger::log( $msg );
+        self::log_value( $var_name, $var, 'Variable' );
     }
-
+    public static function log_param( $var_name, $var )
+    {
+        self::log_value( $var_name, $var, 'Param' );
+    }
+    public static function log_msg( $message )
+    {
+        $message = "------ Message ----- $message";
+        self::log_internal( $message );
+    }
 	public static function log( $message )
     {
-        file_put_contents( Wkwgs_Logger::log_file_name, $message . PHP_EOL, FILE_APPEND );
+        self::log_internal( $message );
+    }
+	protected static function log_internal( $message )
+    {
+        if ( is_null( $message ) )
+        {
+            return;
+        }
+
+        $full_msg = '';
+        foreach( explode( PHP_EOL, $message ) as $text )
+        {
+            if ( ! empty( $text ) )
+            {
+                $full_msg .= self::indent() . $text . PHP_EOL;
+            }
+        }
+        self::save_text_to_file( $full_msg );
     }
 
-    private static function ignore_file_not_exist_error( int $errno , string $errstr, string $errfile, int $errline, array $errcontext )
+	protected static function save_text_to_file( $text )
+    {
+        if ( ! empty( $text ) )
+        {
+            file_put_contents( self::log_file_name, $text, FILE_APPEND );
+        }
+    }
+
+    protected static function ignore_file_not_exist_error( int $errno , string $errstr, string $errfile, int $errline, array $errcontext )
     {
         if (strpos($errstr, 'No such file or directory') !== false)
         {
@@ -84,5 +145,36 @@ class Wkwgs_Logger
         self::log_var( '$errcontext'        , $errcontext       );
         self::log_var( 'error_get_last()'   , error_get_last()  );
         return False;
+    }
+}
+
+class Wkwgs_Function_Logger extends Wkwgs_Logger
+{
+    private $function_name = '';
+
+    public function __construct( $func, $params = null )
+    {
+        $this->function_name = $func;
+        Wkwgs_Logger::$Indent += 2;
+        $this->log_function( $this->function_name, $params );
+    }
+
+    public function __destruct()
+    {
+        $this->log_return( $this->function_name );
+        Wkwgs_Logger::$Indent -= 2;
+    }
+
+    public static function log_var( $var_name, $var )
+    {
+        Wkwgs_Logger::log_var( $var_name, $var );
+    }
+    public static function log_msg( $message )
+    {
+        Wkwgs_Logger::log_msg( $message );
+    }
+	public static function log( $message )
+    {
+        Wkwgs_Logger::log( $message );
     }
 }
